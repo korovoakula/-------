e7e98940b466e9f0028cd0b722360c29904c2acd
korovoakula <bogdankompot45@gmail.com>
2023-03-31 21:28:07 +0300

    ht'cnhfwsz

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from pyrogram import Client, filters
-app = Client("my_account")
+
+api_id=26295378
+api_hash="ec036eac6e153f91abd70a105eb53247"
+
+with Client("my_account", api_id, api_hash) as app:
